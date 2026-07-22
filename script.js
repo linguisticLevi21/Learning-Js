bef6c26c72cd2ef7ed107172ee1f5f336f593b56
linguisticLevi21 <amishaadi0102@gmail.com>
2026-07-22 09:44:05 +0530

    Calculator V1

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+function calculate(a, b, op) {
+  if (op === "+") return a + b;
+  if (op === "-") return a - b;
+  if (op === "*") return a * b;
+  if (op === "/") return a / b;
+}

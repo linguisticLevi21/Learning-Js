8a89ec0b4110c0afb7fa90d9468e9a71ecd99ace
linguisticLevi21 <amishaadi0102@gmail.com>
2026-07-27 16:37:45 +0530

    buffers in nodejs

--- Backend/3/buffers.js
+++ Backend/3/buffers.js
@@ -1,1 +1,11 @@
 // buffers in node js
+const { Buffer } = require("buffer");
+
+const buf = Buffer.alloc(16);
+console.log(buf[1]); // 0
+
+const buf2 = Buffer.from("Hello World");
+console.log(buf2.toString()); // Hello World
+
+const buf3 = Buffer.unalloc(16); // correct
+console.log(buf3[1]); // 0
